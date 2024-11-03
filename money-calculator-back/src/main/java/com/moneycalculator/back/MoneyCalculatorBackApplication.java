e44af0ab94f432d5fb306f12e1660abc3a50f0d9
Sara Andre <hisarandre@gmail.com>
2024-11-03 11:50:59 +0100

    add back

---
+++ money-calculator-back/src/main/java/com/moneycalculator/back/MoneyCalculatorBackApplication.java
@@ -0,0 +1,13 @@
+package com.moneycalculator.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyCalculatorBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyCalculatorBackApplication.class, args);
+	}
+
+}
---
+++ money-calculator-back/src/test/java/com/moneycalculator/back/MoneyCalculatorBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneycalculator.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyCalculatorBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
