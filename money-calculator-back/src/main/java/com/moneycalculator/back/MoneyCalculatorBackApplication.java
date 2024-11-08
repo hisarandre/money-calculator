890bf1be1eb73e35b2a6dc4169a81ecd0e4f27cf
Sara Andre <hisarandre@gmail.com>
2024-11-08 19:36:57 +0100

    add .env config

--- money-calculator-back/src/main/java/com/moneycalculator/back/MoneyCalculatorBackApplication.java
+++ money-calculator-back/src/main/java/com/moneycalculator/back/MoneyCalculatorBackApplication.java
@@ -2,12 +2,19 @@ package com.moneycalculator.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MoneyCalculatorBackApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// Set the DATABASE_URL property in system properties
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_PWD", dotenv.get("DATABASE_PWD"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
 		SpringApplication.run(MoneyCalculatorBackApplication.class, args);
 	}
-
 }
