8da0cdd47a07b39203166ad29942ab527e2134bb
Sara Andre <hisarandre@gmail.com>
2024-11-10 16:07:27 +0100

    add object mapping to transaction and account

--- money-calculator-back/src/main/java/com/moneycalculator/back/models/MapstructMapper.java
+++ money-calculator-back/src/main/java/com/moneycalculator/back/models/MapstructMapper.java
@@ -1,4 +1,22 @@
 package com.moneycalculator.back.models;
 
-public class MapstructMapper {
+import com.moneycalculator.back.dto.AccountLabelFeeDTO;
+import com.moneycalculator.back.dto.TransactionDTO;
+import com.moneycalculator.back.dto.TransactionIdTypeTotalDTO;
+import com.moneycalculator.back.dto.TransactionTotalDTO;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface MapstructMapper {
+
+    Account accountLabelFeeDTOToAccount(AccountLabelFeeDTO accountLabelFeeDTO);
+
+    @Mapping(source = "transaction", target = "transaction")
+    @Mapping(source = "total", target = "total")
+    TransactionTotalDTO transactionToTransactionTotalDTO(Transaction transaction, Double total);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "type", target = "type")
+    @Mapping(source = "total", target = "total")
+    TransactionIdTypeTotalDTO transactionToTransactionIdTypeTotalDTO(Integer id, String type, Double total);
 }
