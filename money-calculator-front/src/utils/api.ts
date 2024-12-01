88780f0cf8edf96fcf9900dcf8f4b20e2692f7e2
Cécilia Nguyen <ngcecilia@orange.fr>
2024-12-01 13:38:18 +0100

    Update API URL environment variable.

--- money-calculator-front/src/utils/api.ts
+++ money-calculator-front/src/utils/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.BASE_URL || "http://localhost:8081/api";
+const BASE_URL = process.env.BASE_URL;
 
 export const ACCOUNT_URL = `${BASE_URL}/account`;
 export const TRANSACTION_URL = `${BASE_URL}/transaction`;
